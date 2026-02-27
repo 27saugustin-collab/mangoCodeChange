998a67818fea9ba39a2e8a235e91bd506f01c1e3
27saugustin-collab <27saugustin@cpsd.us>
2026-02-27 12:46:43 -0500

    Create desktop.sh

---
+++ desktop.sh
@@ -0,0 +1,10 @@
+┌──(kali㉿kali)-[~]
+└─$ xrandr -o inverted
+                                                                             
+┌──(kali㉿kali)-[~]
+└─$ xrandr -o noraml  
+xrandr: -o: invalid argument 'noraml'
+Try 'xrandr --help' for more information.
+                                                                             
+┌──(kali㉿kali)-[~]
+└─$ xrandr -o normal
